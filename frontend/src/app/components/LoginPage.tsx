e0e0e77d55a02c517b9a0e78b87d6a77dcdde7bd
nisthadash <nisthapradeepdash@gmail.com>
2026-07-07 17:30:49 +0530

    Fix build: replace missing svgPaths imports with inline SVGs for social icons

--- frontend/src/app/components/LoginPage.tsx
+++ frontend/src/app/components/LoginPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { motion } from "motion/react";
-import svgPaths from "../../imports/svg-uvuwjcjnes";
+
 
 export function LoginPage() {
   const navigate = useNavigate();
@@ -105,10 +105,8 @@ export function LoginPage() {
                   className="absolute right-4 top-1/2 -translate-y-1/2"
                 >
                   <div className="relative size-[18px]">
-                    <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 18 18">
-                      <g>
-                        <path d={svgPaths.p1d230e00} fill="white" />
-                      </g>
+                    <svg viewBox="0 0 24 24" width="18" height="18" fill="white" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
                     </svg>
                   </div>
                 </button>
@@ -122,12 +120,10 @@ export function LoginPage() {
                   className="relative size-[18px]"
                 >
                   {rememberMe ? (
-                    <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 18 18">
-                      <g>
-                        <path d={svgPaths.pff37800} fill="url(#paint0_linear_remember)" />
-                      </g>
+                    <svg viewBox="0 0 24 24" width="18" height="18" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" fill="url(#paint0_linear_remember)" />
                       <defs>
-                        <linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_remember" x1="9" x2="9" y1="2.25" y2="15.75">
+                        <linearGradient id="paint0_linear_remember" x1="12" x2="12" y1="3" y2="21" gradientUnits="userSpaceOnUse">
                           <stop stopColor="#7CC1F3" />
                           <stop offset="1" stopColor="#D27EEF" />
                         </linearGradient>
@@ -214,42 +210,24 @@ function SocialIcon({ type }: { type: "google" | "facebook" | "github" }) {
     <motion.button
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      className="relative size-[42px] cursor-pointer"
+      className="relative size-[42px] cursor-pointer rounded-full flex items-center justify-center overflow-hidden"
     >
       {type === "google" && (
-        <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 42 42">
-          <g clipPath="url(#clip0_google)">
-            <path d={svgPaths.p17249700} fill="white" />
-            <path d={svgPaths.p6eed480} fill="#E33629" />
-            <path d={svgPaths.p2051fc00} fill="#F8BD00" />
-            <path d={svgPaths.p36f9a900} fill="#587DBD" />
-            <path d={svgPaths.p4fd1480} fill="#319F43" />
-          </g>
-          <defs>
-            <clipPath id="clip0_google">
-              <rect fill="white" height="42" width="42" />
-            </clipPath>
-          </defs>
+        <svg viewBox="0 0 24 24" width="28" height="28" xmlns="http://www.w3.org/2000/svg">
+          <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+          <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
+          <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+          <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
         </svg>
       )}
       {type === "facebook" && (
-        <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 42 42">
-          <g clipPath="url(#clip0_facebook)">
-            <path d={svgPaths.p17708a00} fill="#1877F2" />
-            <path d={svgPaths.p984ba00} fill="white" />
-          </g>
-          <defs>
-            <clipPath id="clip0_facebook">
-              <rect fill="white" height="42" width="42" />
-            </clipPath>
-          </defs>
+        <svg viewBox="0 0 24 24" width="28" height="28" xmlns="http://www.w3.org/2000/svg">
+          <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" fill="#1877F2" />
         </svg>
       )}
       {type === "github" && (
-        <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 42 42">
-          <g>
-            <path d={svgPaths.p18209680} fill="white" />
-          </g>
+        <svg viewBox="0 0 24 24" width="28" height="28" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.09.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.155-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.741 0 .267.18.578.688.48C19.138 20.167 22 16.42 22 12c0-5.523-4.477-10-10-10z" fill="white" />
         </svg>
       )}
     </motion.button>
